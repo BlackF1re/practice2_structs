8e4df091b4a3a90c1176d2b710de705ac321c9ed
Павел Левицкий <creative-creative@outlook.com>
2024-03-22 13:50:35 +0700

    upd

--- Source.c
+++ Source.c
@@ -4,36 +4,67 @@
 //
 //По запросу покупателя выдавать сведения о наличии лекарства, при его отсутствии - список заменителей, имеющихся в наличии.
 //Использовать структурные переменные.
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <locale.h>
 
 typedef struct {
     int id;
     char name[50];
     float cost;
-    int isExists;
+    int is_exists;
     char generics[100];
 } Drug;
 
-int main() {
-    FILE* fp = fopen("drugs.txt", "r");
+void read_drugs_from_file(const char* filename, Drug** drugs, int* count) {
+    FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
-        perror("Ошибка открытия файла");
-        return EXIT_FAILURE;
+        perror("Error opening file");
+        return;
     }
 
-    //total output:
+    *drugs = NULL;
+    *count = 0;
+
     Drug drg;
-    while (fscanf(fp, "%d %s %lf %d %s", &drg.id, drg.name, &drg.cost, &drg.isExists, drg.generics) != EOF) {
-        printf("ID: %d\n", drg.id);
-        printf("Название: %s\n", drg.name);
-        printf("Стоимость: %.2f\n\n", drg.cost);
-        printf("Наличие: %d\n", drg.isExists);
-        printf("Аналоги: %s\n", drg.generics);
+    while (fscanf(fp, "%d %s %f %d %s", &drg.id, drg.name, &drg.cost, &drg.is_exists, drg.generics) != EOF) {
+        *count = *count + 1;
+        *drugs = realloc(*drugs, *count * sizeof(Drug));
+        if (*drugs == NULL) {
+            perror("Error allocating memory");
+            return;
+        }
+        (*drugs)[*count - 1] = drg;
     }
-
     fclose(fp);
-    //some magic
+}
+
+void print_drugs(Drug* drugs, int count) {
+    for (int i = 0; i < count; i++) {
+        printf("ID:\t%d\n", drugs[i].id);
+        printf("Название:\t%s\n", drugs[i].name);
+        printf("Стоимость:\t%.2f\n", drugs[i].cost);
+        if(drugs[i].is_exists == 1)
+            printf("Наличие:\tДа.\n");
+        else
+        {
+            printf("Наличие:\tНет.\n");
+        }
+
+        printf("Аналоги:\t%s\n", drugs[i].generics);
+        printf("--------------------------------------------------\n");
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    Drug* drugs = NULL;
+    int count = NULL;
+
+    read_drugs_from_file("drugs.txt", &drugs, &count);
+    print_drugs(drugs, count);
+
+    free(drugs);
     return EXIT_SUCCESS;
 }
